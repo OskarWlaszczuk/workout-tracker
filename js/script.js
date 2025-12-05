60762b43dccd2cc4954a5d1bebd52afb8ec831fe
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-12-05 18:57:18 +0100

    call Workout component fn on specific client url in init function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-import { ExerciseItemElement, ExercisesListElement, ExerciseTab, WorkoutNameElement } from "./elements.js";
-import { formatToKebabCase } from "./utils/formatToKebabCase.js";
-import { fetchWorkout } from "./utils/fetchWorkout.js";
+import { Workout } from "./Components/Workout.js";
 //   // const toggleOpenButtonElement = dynaminToggleOpenButtonElement(buttonIndex);
 //         console.log(toggleOpenButton);
 
@@ -49,110 +47,120 @@ import { fetchWorkout } from "./utils/fetchWorkout.js";
 
 
 {
-    const dynaminToggleOpenButtonElement = (buttonIndex) => {
-        const toggleOpenButtonElements = Array.from(document.querySelectorAll(".js-toggleExerciseOpenButton"));
-        const toggleOpenButtonElement = toggleOpenButtonElements.find((_, index) => index === buttonIndex);
+//     const dynaminToggleOpenButtonElement = (buttonIndex) => {
+//         const toggleOpenButtonElements = Array.from(document.querySelectorAll(".js-toggleExerciseOpenButton"));
+//         const toggleOpenButtonElement = toggleOpenButtonElements.find((_, index) => index === buttonIndex);
 
-        return toggleOpenButtonElement;
-    };
+//         return toggleOpenButtonElement;
+//     };
 
-    //rozdzielić funkcje renderujące na warstwy - HTML tag (template strings, dodawanie danych dynamicznych) -> render (dodanie tagu do DOM)
+//     //rozdzielić funkcje renderujące na warstwy - HTML tag (template strings, dodawanie danych dynamicznych) -> render (dodanie tagu do DOM)
 
-    const toggleExerciseOpen = (
-        // toggleOpenButton,
-        // buttonIndex,
-        exercises
-    ) => {
-        return (event => {
-            console.log(exercises, event.target);
-            const clickedButton = event.target;
-            const isClosed = clickedButton.textContent === "Open";
-            const clickedExercise = exercises.find((_, index) => index === buttonIndex);
-
-
-            const exerciseTab = document.getElementById(formatToKebabCase([...clickedExercise.name.split(" "), "tab"]));
-
-            if (isClosed) {
-                //wyświetlenie exercise tab
-                //jeśli istnieje to znaczy, że było wcześniej wyświetlane i jest tylko ukryte - trzeba wyciągnąć ten element dynamicznie z DOMu
-                //jeśli nie to znaczy, że user otwiera pierwszy raz zakładkę i trzeba stworzyć statyczny string HTML i dodać go do DOMu
-            }
-        });
-    };
-
-    const toggleExerciseOpen2 = (
-        // toggleOpenButton,
-        // buttonIndex,
-        exercises,
-        buttonIndex
-    ) => {
-        return (event => {
-            console.log(exercises, event.target);
-
-            const clickedButton = event.target;
-            const isClosed = clickedButton.textContent === "Open";
-            const clickedExercise = exercises.find((_, index) => index === buttonIndex);
-
-            console.log(document.createElement("a"));
-
-            // const exerciseTab = document.getElementById(formatToKebabCase([...clickedExercise.name.split(" "), "tab"]));
-
-            if (isClosed) {
-                //wyświetlenie exercise tab
-                //jeśli istnieje to znaczy, że było wcześniej wyświetlane i jest tylko ukryte - trzeba wyciągnąć ten element dynamicznie z DOMu
-                //jeśli nie to znaczy, że user otwiera pierwszy raz zakładkę i trzeba stworzyć statyczny string HTML i dodać go do DOMu
-
-                // if (exerciseTab) {
-                //     exerciseTab.style.display = "block"
-                // } else {
+//     const toggleExerciseOpen = (
+//         // toggleOpenButton,
+//         // buttonIndex,
+//         exercises
+//     ) => {
+//         return (event => {
+//             console.log(exercises, event.target);
+//             const clickedButton = event.target;
+//             const isClosed = clickedButton.textContent === "Open";
+//             const clickedExercise = exercises.find((_, index) => index === buttonIndex);
 
 
-                // }
-            }
+//             const exerciseTab = document.getElementById(formatToKebabCase([...clickedExercise.name.split(" "), "tab"]));
+
+//             if (isClosed) {
+//                 //wyświetlenie exercise tab
+//                 //jeśli istnieje to znaczy, że było wcześniej wyświetlane i jest tylko ukryte - trzeba wyciągnąć ten element dynamicznie z DOMu
+//                 //jeśli nie to znaczy, że user otwiera pierwszy raz zakładkę i trzeba stworzyć statyczny string HTML i dodać go do DOMu
+//             }
+//         });
+//     };
+
+//     const toggleExerciseOpen2 = (
+//         // toggleOpenButton,
+//         // buttonIndex,
+//         exercises,
+//         buttonIndex
+//     ) => {
+//         return (event => {
+//             console.log(exercises, event.target);
+
+//             const clickedButton = event.target;
+//             const isClosed = clickedButton.textContent === "Open";
+//             const clickedExercise = exercises.find((_, index) => index === buttonIndex);
+
+//             console.log(document.createElement("a"));
+
+//             // const exerciseTab = document.getElementById(formatToKebabCase([...clickedExercise.name.split(" "), "tab"]));
+
+//             if (isClosed) {
+//                 //wyświetlenie exercise tab
+//                 //jeśli istnieje to znaczy, że było wcześniej wyświetlane i jest tylko ukryte - trzeba wyciągnąć ten element dynamicznie z DOMu
+//                 //jeśli nie to znaczy, że user otwiera pierwszy raz zakładkę i trzeba stworzyć statyczny string HTML i dodać go do DOMu
+
+//                 // if (exerciseTab) {
+//                 //     exerciseTab.style.display = "block"
+//                 // } else {
 
 
-        });
-    };
-
-    const bindToggleExerciseOpenEvents = (exercises) => {
-        // const exerciseList = document.querySelector(".js-workout-exercises");
-        // exerciseList.addEventListener("click", toggleExerciseOpen(exercises))
+//                 // }
+//             }
 
 
-        const buttons = document.querySelectorAll(".js-toggleExerciseOpenButton");
+//         });
+//     };
 
-        buttons.forEach((button, buttonIndex) => {
-            button.addEventListener("click", toggleExerciseOpen2(exercises, buttonIndex));
-        });
-    };
+//     const bindToggleExerciseOpenEvents = (exercises) => {
+//         // const exerciseList = document.querySelector(".js-workout-exercises");
+//         // exerciseList.addEventListener("click", toggleExerciseOpen(exercises))
 
-    //funkcje renderujące - manipulacja DOMem
-    const renderWorkoutExercisesElement = (exercises) => {
-        //użyć appendChild - praca na nodes
-        document.querySelector(".js-workout-exercises").innerHTML = ExercisesListElement(exercises);
-    };
 
-    const renderWorkoutNameElement = (name) => {
-        //czy warto dodawać warstwę walidacji, która będzie sprawdzać istnienie pojedynczych fetchowanych danych?
-        // na wypadedk, gdyby pojedyncze własćiwości odpowiedzi nie były dostępne w response?
-        //czy może wystarczy sprawdzenie tylko obiektu i statusu odpowiedzi?
-        if (!name) {
-            document.querySelector(".js-workout-name").innerHTML = WorkoutNameElement("Name does not provided");
-        }
+//         const buttons = document.querySelectorAll(".js-toggleExerciseOpenButton");
 
-        //dodawanie elementu do HTML, przy użyciu querySelector w bardziej immutability sposób?
-        document.querySelector(".js-workout-name").innerHTML = WorkoutNameElement(name);
-    };
+//         buttons.forEach((button, buttonIndex) => {
+//             button.addEventListener("click", toggleExerciseOpen2(exercises, buttonIndex));
+//         });
+//     };
+
+//     //funkcje renderujące - manipulacja DOMem
+//     const renderWorkoutExercisesElement = (exercises) => {
+//         //użyć appendChild - praca na nodes
+//         document.querySelector(".js-workout-exercises").innerHTML = ExercisesListElement(exercises);
+//     };
+
+//     const renderWorkoutNameElement = (name) => {
+//         //czy warto dodawać warstwę walidacji, która będzie sprawdzać istnienie pojedynczych fetchowanych danych?
+//         // na wypadedk, gdyby pojedyncze własćiwości odpowiedzi nie były dostępne w response?
+//         //czy może wystarczy sprawdzenie tylko obiektu i statusu odpowiedzi?
+//         if (!name) {
+//             document.querySelector(".js-workout-name").innerHTML = WorkoutNameElement("Name does not provided");
+//         }
+
+//         //dodawanie elementu do HTML, przy użyciu querySelector w bardziej immutability sposób?
+//         document.querySelector(".js-workout-name").innerHTML = WorkoutNameElement(name);
+//     };
+
 
     const init = async () => {
-        const workout = await fetchWorkout();
+        const userUrlLocation = window.location.pathname;
 
-        renderWorkoutNameElement(workout.name);
-        renderWorkoutExercisesElement(workout.exercises);
-        // workout.exercises.forEach(ex => {
-        //     console.log(ExerciseTab(ex));
-        // })
-        bindToggleExerciseOpenEvents(workout.exercises);
+        switch (userUrlLocation) {
+            case "/index.html":
+                return Workout();
+            default:
+                break;
+        }
+
+        // const workout = await fetchWorkout();
+        // ExerciseTab()
+        // renderWorkoutNameElement(workout.name);
+        // renderWorkoutExercisesElement(workout.exercises);
+        // // workout.exercises.forEach(ex => {
+        // //     console.log(ExerciseTab(ex));
+        // // })
+        // bindToggleExerciseOpenEvents(workout.exercises);
     };
 
     init();
