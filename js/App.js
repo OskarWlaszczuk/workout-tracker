2c7253d8c515768cafd5f383f276c4354b65febb
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-12-22 12:57:50 +0100

    replace Workout component with Paragraph for index.html rendering

--- js/App.js
+++ js/App.js
@@ -1,11 +1,11 @@
-import { Workout } from "./Components/Workout.js";
+import { Paragraph } from "./Components/Workout.js";
 
 export const App = () => {
     const userUrlLocation = window.location.pathname;
 
     switch (userUrlLocation) {
         case "/index.html":
-            return Workout();
+            return Paragraph({ content: "text" });
         default:
             break;
     }
