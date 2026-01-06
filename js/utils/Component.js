1ff7fbbd281c715b637244f4356442f07e635e35
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2026-01-06 15:26:11 +0100

    const generateNodes = () => {             const generateNodes = () => {             const generateNodes = () => { Renamed states constructor prop of Component to statesMap because I have changed its initial value to Map class instance. Now searching and managing specif statesMap element easier by using its key. Removed emitStateChange method. Create unique key identifier for each component state on useState call during component mount. The key is strictlly tied to useStates call order, because it helps to automatically find the right state value on component re-render. Added assignProps and generateNodes functions inside the init method. These fuctions are used to create root structure based on nodesConfigMap returned by component function call. This structure is ready to be injected directly into DOM.

--- js/utils/Component.js
+++ js/utils/Component.js
@@ -1,105 +1,151 @@
-import { render } from "../render.js";
+function* idGenerator(initialId, isUpdating) {
+    while (!isUpdating) {
+        yield initialId;
+        initialId++
+    }
+};
 export class Component {
     constructor(componentFunction) {
         this.componentFunction = componentFunction;
-        this.states = [];
+        this.statesMap = new Map();
         this.props = {};
         this.isUpdating = false;
-        this.useState = (defaultValue) => {
-            //automatyczne stworzenie miejsca w pamięci dla stanu po wywołaniu hooka
-            const newState = { id: this.states.length + 1, value: defaultValue };
+        this.updatingStates = new Map();
+        this.array = [];
 
-            //jeżeli flaga isUpdating jest true to znaczy, że funkcja komponentu zostaje ponownie wywołana, aby zaktualizować widok
-            //w tej sytuacji useState nie wywołuje się ponownie, aby przydzielić ponownie tych samych stanów
-            if (this.isUpdating) {
-                return;
+        this.useState = (initialStateValue) => {
+            const stateId = this.isUpdating ? this.array.length : this.statesMap.size;
+
+            if (!this.isUpdating) {
+                this.statesMap.set(stateId, initialStateValue);
             }
 
-            this.states = [...this.states, newState];
-            //znajdywanie odpowiedniego stanu, na podstawie kolejności utworzenia stanu (wywołaniu useState)
-            //wewnątrz funkcji komponentu
-
-            const currentState = this.states.find(({ id }) => id === newState.id).value;
-
             const setState = (newStateValue) => {
-                //czy odnośnik do konstruktora klasy będzie działał, jeżeli wywołam setState wewnątrz funkcji komponentu?
-                this.emitStateChange(newState.id, newStateValue);
+
+                console.log(`Updating state ${stateId}`);
+                this.updatingStates.set(stateId, newStateValue);
+                this.isUpdating = true;
             };
+
+            const currentState = this.statesMap.get(stateId);
 
             return [currentState, setState];
         };
+
         this.data = this.init();
-        this.elementsMap = new Map();
+        this.nodesMap = new Map();
         this.rootElement = undefined;
     };
-    //tutaj generowanie elementów na podstawie mapy obiektów 
-    init() {
-        console.log("init..");
 
+    init() {
         return (props) => {
             this.props = props;
             //obecnie stworzyłem uproszczoną wersję w której funkcja komponentu od razu zwraca sparsowany widok tablicy tablic
             //zamiast HTML string
-            const parsedElementsObjectMap = this.componentFunction({ component: this, ...props });
-            const elementsConfigMap = new Map(parsedElementsObjectMap);
+            const nodesConfig = this.componentFunction({ component: { ...this }, ...this.props });
+            const nodesConfigMap = new Map(nodesConfig);
 
+            const availableProps = new Map([
+                ["onClick", { type: "event", eventName: "click" }],
+                ["class", { type: "attribute" }],
+            ]);
 
-            //generowanie elementówna podstawie ich danych
-            elementsConfigMap.forEach((_, elementKey) => {
-                const elementData = elementsConfigMap.get(elementKey);
+            const assignProps = (element, props) => {
+                props.forEach((propValue, propKey) => {
+                    const propData = availableProps.get(propKey);
 
-                const element = document.createElement(elementData.type);
+                    if (propData.type === "event") {
 
-                this.elementsMap.set(elementKey, element);
-            });
+                        const dispatchStateChange = () => {
+                            console.log("dispatching state change");
+
+                            //update zaznaczonych stanów i re-render komponentu
+                            console.log(this.updatingStates.entries(), this.statesMap.entries());
+
+                            this.updatingStates.forEach((value, key) => {
+                                this.statesMap.set(key, value);
+                            });
+                            console.log("states map after updates:", this.statesMap);
+
+                            this.callComponent();
+                            this.updatingStates.clear();
+                            this.array = []
+                        };
+
+                        const eventHandlerWrapper = () => {
+                            return (eventObject) => {
+                                propValue(eventObject);
+
+                                if (this.isUpdating) {
+                                    dispatchStateChange();
+                                }
+                            };
+                        };
+
+                        element.addEventListener(propData.eventName, eventHandlerWrapper());
+                    }
+                    else if (propData.type === "attribute") {
+                        element.setAttribute(propKey, propValue);
+                    }
+
+                });
+            };
+
+            const generateNodes = () => {
+                //generowanie elementówna podstawie ich danych
+                nodesConfigMap.forEach((_, elementKey) => {
+                    const elementData = nodesConfigMap.get(elementKey);
+
+                    if (elementData.type === "text") {
+                        const textNode = document.createTextNode(elementData.content);
+                        this.nodesMap.set(elementKey, textNode);
+                        return;
+                    }
+
+                    const elementNode = document.createElement(elementData.type);
+
+                    if (elementData.props) {
+                        assignProps(elementNode, elementData.props)
+                    }
+
+                    this.nodesMap.set(elementKey, elementNode);
+                });
+            };
 
             const buildFromRoot = () => {
-                elementsConfigMap.forEach((data, key) => {
-                    const element = this.elementsMap.get(key);
+                nodesConfigMap.forEach((data, key) => {
+                    const element = this.nodesMap.get(key);
 
                     const parentElementKey = data.parent;
 
-                    if (!parentElementKey) {
-                        console.log("Root element found:", data);
+                    if (!parentElementKey && data.type !== "text") {
                         this.rootElement = element;
                         return
                     }
 
-                    const parentElement = this.elementsMap.get(parentElementKey);
+                    const parentElement = this.nodesMap.get(parentElementKey);
 
                     parentElement.append(element);
                 })
             };
 
+            generateNodes();
             buildFromRoot();
 
             return this.rootElement;
         }
-    }
-
-    emitStateChange(stateId, newStateValue) {
-        //metoda do wywołania, gdy chcę zaktualizować stan
-        const changedState = this.states.find(({ id }) => id === stateId);
-
-        //zmiana pojedynczego stanu
-        this.states = [
-            ...this.states.filter(({ id }) => id !== stateId),
-            {
-                ...changedState,
-                value: newStateValue,
-            },
-        ];
-
-        //ustawienie isUpdating na true, pozwoli pominąć ponowne wywołania useState wewnątrz funkcji komponentu
-        this.isUpdating = true;
-        this.callComponent();
     };
 
     callComponent() {
-        //uzyskanie aktualnego HTML string komponentu po zmianie stanu lub props
-        this.componentFunction(this);
+        console.log("calling component");
 
-        //wywołanie funkcji, która odświeży widok strony
-        render();
+        //uzyskanie aktualnego HTML string komponentu po zmianie stanu lub props
+        const componentInput = { component: { ...this }, ...this.props };
+        const comp = new Map(this.componentFunction(componentInput));
+        //zmiana stanu powoduje re-render komponentu i aktualizację jego drzewa, ale póki co zmiany nie są nanoszone na DOM
+        //po re-renderze i wygenerowaniu zaktuazlizowanego drzewa musi nastąpić proces diffingu
+        //w jaki sposób łatwo porównać ze sobą oba drzewa i znaleźć różnicę?
+
+        console.log("re-call component:", Array.from(comp), "current nodes:", this.nodesMap, this.rootElement.childNodes);
     };
 };
